6ad2b82f694c031d6c993010578708ee0d09dde7
Safwen <safwen.debbichi.91@gmail.com>
2024-06-01 14:28:34 +0200

    Initial Commit

---
+++ EnvironmentValueExample/EnvironmentValueExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentValueExampleApp.swift
+//  EnvironmentValueExample
+//
+//  Created by Safwen DEBBICHI on 01/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentValueExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
